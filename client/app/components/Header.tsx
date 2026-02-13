72bf94cf6876f11a92c9bb26a4df794f28d23d3e
Shekhar Singh Kushwah <kushwahshekhar.singh44@gmail.com>
2026-02-13 21:23:57 +0530

    Added Basic Navbar Layout

---
+++ client/app/components/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+export default function Header(){
+    return(
+        <header>
+          <div className="container items-center flex justify-between py-4 px-6 mx-auto">
+          <Link href={'/'} className="font-bold text-2xl">Job board</Link>
+
+          <nav className="flex gap-2  *:py-2 *:px-4 *:rounded-md">
+          <Link href={'/login'} className="bg-gray-200">Login</Link>
+          <Link className="bg-blue-600 text-white " href={'/new-listing'}>Post a Job</Link>
+        </nav>
+        </div>
+        </header>
+    )
+}
